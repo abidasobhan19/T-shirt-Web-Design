349e7cfe984b3beb28e305bef43b4e9fdc3d1d12
abidasobhan19 <abidasobhan19@gmail.com>
2022-04-04 16:17:23 +0600

    new update

--- src/component/Design/canvas.js
+++ src/component/Design/canvas.js
@@ -3,34 +3,30 @@ import { fabric } from "fabric";
 import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
 import Myimg from "../../img/a.jpg";
 const Cantest = () => {
-  const [fdata, setFata] = useState();
-  //   const { editor, onReady } = useFabricJSEditor();
-  // const onAddCircle = () => {
-  //   editor?.addCircle();
-  // };
   const { selectedObjects, editor, onReady } = useFabricJSEditor();
-  useEffect(() => {
-    fabric.Image.fromURL(Myimg, function (oImg) {
-      oImg.scaleToHeight(100);
-      oImg.scaleToWidth(100);
-      editor.canvas.add(oImg);
-      // console.log(oImg);
-      setFata(oImg);
-    });
+  // useEffect(() => {
+  //   fabric.Image.fromURL(Myimg, function (oImg) {
+  //     oImg.scaleToHeight(100);
+  //     oImg.scaleToWidth(100);
+  //     editor.canvas.add(oImg);
+  //   });
+  // });
+
+  fabric.Image.fromURL(Myimg, function (oImg) {
+    oImg.scaleToHeight(100);
+    oImg.scaleToWidth(140);
+    editor.canvas.add(oImg);
+    console.log(oImg);
   });
-  const onAddRectangle = () => {
-    console.log(fdata);
-    var data = editor.canvas.toDataURL(fdata);
-    window.open(data);
-  };
+
   return (
     <div>
       <div>
-        <FabricJSCanvas className="sample-canvas" onReady={onReady} />
-        <div>
-          {/* <button onClick={onAddCircle}>Add circle</button> */}
-          <button onClick={onAddRectangle}>Save</button>
-        </div>
+        <FabricJSCanvas
+          id="canvas"
+          className="sample-canvas"
+          onReady={onReady}
+        />
       </div>
     </div>
   );
--- src/component/Design/display.js
+++ src/component/Design/display.js
@@ -1,24 +1,44 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import BlackTshirt from "../../img/T-shirt Layout/T-shirt.png";
 import Canvastest from "./canvas";
+import domtoimage from "dom-to-image";
 const Display = () => {
+  const getimage = () => {
+    console.log("hello");
+    var node = document.getElementById("my-node");
+
+    domtoimage
+      .toPng(node)
+      .then(function (dataUrl) {
+        var img = new Image();
+        img.src = dataUrl;
+        console.log(dataUrl);
+        document.body.appendChild(img);
+      })
+      .catch(function (error) {
+        console.error("oops, something went wrong!", error);
+      });
+  };
+
   return (
-    <Box display="flex" flexGrow="1">
+    <Box id="my-node" display="flex" flexGrow="1">
       <Box style={{ marginTop: 80, marginLeft: "25%" }}>
         <img style={{ fillColor: "black" }} src={BlackTshirt} alt="t-shirt" />
       </Box>
-
       <Box
         style={{
-          width: 130,
+          width: 150,
           height: 100,
           marginTop: 150,
-          marginLeft: -205,
-          backgroundColor: "yellow",
+          marginLeft: -210,
         }}
       >
         <Canvastest />
+      </Box>
+
+      <Box>
+        <Button onClick={getimage}> Click to Save</Button>
       </Box>
     </Box>
   );
